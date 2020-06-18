4f65630028a5b7afe6b3d6789cb6897d2fcd7497
KMiller67 <41657618+KMiller67@users.noreply.github.com>
2020-06-18 15:18:29 -0500

    Added modeling portion and code cleanup
    Added the modeling portion that had been completed in related Kaggle post. Modeling code included as a function instead for simplified code. Finished cleaning up code to be more 'production' style by removing exploratory code (with the exception of graphs).

--- main.py
+++ main.py
@@ -3,41 +3,47 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
 
 
 def main():
 
     # Load data using Path, making it easy to use on different operating systems
-    SBA_data = pd.read_csv(Path().joinpath('should-this-loan-be-approved-or-denied', 'SBAnational.csv'))
+    df = pd.read_csv(Path().joinpath('should-this-loan-be-approved-or-denied', 'SBAnational.csv'))
 
     # Copies data frame to use for exploration purposes
-    SBA_data_copy = SBA_data.copy()
+    df_copy = df.copy()
 
     # Data cleaning, formatting and feature engineering
     # Remove all records with a null value
-    SBA_data_copy.dropna(subset=['Name', 'City', 'State', 'BankState', 'NewExist',
-                                 'RevLineCr', 'LowDoc', 'DisbursementDate', 'MIS_Status'], inplace=True)
+    df_copy.dropna(subset=['Name', 'City', 'State', 'BankState', 'NewExist', 'RevLineCr', 'LowDoc', 'DisbursementDate',
+                           'MIS_Status'], inplace=True)
 
     # Remove '$', commas, and extra spaces from records in columns with dollar values that should be floats
-    SBA_data_copy[['DisbursementGross', 'BalanceGross', 'ChgOffPrinGr', 'GrAppv', 'SBA_Appv']] = \
-        SBA_data_copy[['DisbursementGross', 'BalanceGross', 'ChgOffPrinGr', 'GrAppv', 'SBA_Appv']].applymap(
-            lambda x: x.strip().replace('$', '').replace(',', ''))
+    df_copy[['DisbursementGross', 'BalanceGross', 'ChgOffPrinGr', 'GrAppv', 'SBA_Appv']] = \
+        df_copy[['DisbursementGross', 'BalanceGross', 'ChgOffPrinGr', 'GrAppv', 'SBA_Appv']].applymap(
+        lambda record: record.strip().replace('$', '').replace(',', ''))
 
     # A few records in ApprovalFY are listed as 1976A; Remove the 'A' from these records for ease of formatting
     # ApprovalFY has multiple data types
-    SBA_data_copy['ApprovalFY'].apply(type).value_counts()
-    SBA_data_copy['ApprovalFY'] = SBA_data_copy['ApprovalFY'].apply(clean_str).astype('int64')
+    df_copy['ApprovalFY'] = df_copy['ApprovalFY'].apply(clean_str).astype('int64')
 
     # Change the type of NewExist to an integer, Zip and UrbanRural to str (categorical)
     # and all currency-related fields to float values
-    SBA_data_copy = SBA_data_copy.astype(
+    df_copy = df_copy.astype(
         {'Zip': 'str', 'NewExist': 'int64', 'UrbanRural': 'str', 'DisbursementGross': 'float',
          'BalanceGross': 'float', 'ChgOffPrinGr': 'float', 'GrAppv': 'float',
          'SBA_Appv': 'float'})
 
     # Create a new column with the industry the NAICS code represents
-    SBA_data_copy['Industry'] = SBA_data_copy['NAICS'].astype('str').apply(lambda x: x[:2])
-    SBA_data_copy['Industry'] = SBA_data_copy['Industry'].map({
+    df_copy['Industry'] = df_copy['NAICS'].astype('str').apply(lambda x: x[:2])
+    df_copy['Industry'] = df_copy['Industry'].map({
         '11': 'Ag/For/Fish/Hunt',
         '21': 'Min/Quar/Oil_Gas_ext',
         '22': 'Utilities',
@@ -65,107 +71,94 @@ def main():
     })
 
     # Remove records where Industry is NaN (NAICS code was a 0)
-    SBA_data_copy.dropna(subset=['Industry'], inplace=True)
+    df_copy.dropna(subset=['Industry'], inplace=True)
 
     # Create flag column IsFranchise based on FranchiseCode column
-    SBA_data_copy.loc[(SBA_data_copy['FranchiseCode'] <= 1), 'IsFranchise'] = 0
-    SBA_data_copy.loc[(SBA_data_copy['FranchiseCode'] > 1), 'IsFranchise'] = 1
+    df_copy.loc[(df_copy['FranchiseCode'] <= 1), 'IsFranchise'] = 0
+    df_copy.loc[(df_copy['FranchiseCode'] > 1), 'IsFranchise'] = 1
 
     # Adjust current flag identifiers for NewExist, RevLineCr, LowDoc, and MIS_Status
     # NewExist
     # Make sure NewExist has only 1s and 2s; Remove records where NewExist isn't 1 or 2
-    SBA_data_copy['NewExist'].unique()
-    SBA_data_copy = SBA_data_copy[(SBA_data_copy['NewExist'] == 1) | (SBA_data_copy['NewExist'] == 2)]
+    df_copy = df_copy[(df_copy['NewExist'] == 1) | (df_copy['NewExist'] == 2)]
 
     # NewExist - 0 = Existing business, 1 = New business; Renamed to NewBusiness
-    SBA_data_copy.loc[(SBA_data_copy['NewExist'] == 1), 'NewBusiness'] = 0
-    SBA_data_copy.loc[(SBA_data_copy['NewExist'] == 2), 'NewBusiness'] = 1
+    df_copy.loc[(df_copy['NewExist'] == 1), 'NewBusiness'] = 0
+    df_copy.loc[(df_copy['NewExist'] == 2), 'NewBusiness'] = 1
 
     # RevLineCr and LowDoc
-    # Double check RevLineCr and LowDoc unique values
-    SBA_data_copy['RevLineCr'].unique()
-    SBA_data_copy['LowDoc'].unique()
-
     # Remove records where RevLineCr != 'Y' or 'N' and LowDoc != 'Y' or 'N'
-    SBA_data_copy = SBA_data_copy[(SBA_data_copy['RevLineCr'] == 'Y') | (SBA_data_copy['RevLineCr'] == 'N')]
-    SBA_data_copy = SBA_data_copy[(SBA_data_copy['LowDoc'] == 'Y') | (SBA_data_copy['LowDoc'] == 'N')]
+    df_copy = df_copy[(df_copy['RevLineCr'] == 'Y') | (df_copy['RevLineCr'] == 'N')]
+    df_copy = df_copy[(df_copy['LowDoc'] == 'Y') | (df_copy['LowDoc'] == 'N')]
 
     # RevLineCr and LowDoc - 0 = No, 1 = Yes
-    SBA_data_copy['RevLineCr'] = np.where(SBA_data_copy['RevLineCr'] == 'N', 0, 1)
-    SBA_data_copy['LowDoc'] = np.where(SBA_data_copy['LowDoc'] == 'N', 0, 1)
+    df_copy['RevLineCr'] = np.where(df_copy['RevLineCr'] == 'N', 0, 1)
+    df_copy['LowDoc'] = np.where(df_copy['LowDoc'] == 'N', 0, 1)
 
     # Make Default target field based on MIS_Status so P I F = 1 and CHGOFF = 0
     # Allows us to see what features are prevalent in a defaulted loan
-    SBA_data_copy['Default'] = np.where(SBA_data_copy['MIS_Status'] == 'P I F', 0, 1)
+    df_copy['Default'] = np.where(df_copy['MIS_Status'] == 'P I F', 0, 1)
 
     # Convert ApprovalDate and DisbursementDate columns to datetime values
     # ChgOffDate not changed to datetime since it is not of value and will be removed later
-    SBA_data_copy[['ApprovalDate', 'DisbursementDate']] = SBA_data_copy[['ApprovalDate', 'DisbursementDate']].apply(
-        pd.to_datetime)
+    df_copy[['ApprovalDate', 'DisbursementDate']] = df_copy[['ApprovalDate', 'DisbursementDate']].apply(pd.to_datetime)
 
     # Create DisbursementFY field for time selection criteria later
-    SBA_data_copy['DisbursementFY'] = SBA_data_copy['DisbursementDate'].map(lambda x: x.year)
+    df_copy['DisbursementFY'] = df_copy['DisbursementDate'].map(lambda record: record.year)
 
     # Create DaysToDisbursement column which calculates the number of days passed between DisbursementDate and
     # ApprovalDate
-    SBA_data_copy['DaysToDisbursement'] = SBA_data_copy['DisbursementDate'] - SBA_data_copy['ApprovalDate']
+    df_copy['DaysToDisbursement'] = df_copy['DisbursementDate'] - df_copy['ApprovalDate']
 
     # Change DaysToDisbursement from a timedelta64 dtype to an int64 dtype
     # Converts series to str, removes all characters after the space before 'd' in days for each record, then changes
     # the dtype to int
-    SBA_data_copy['DaysToDisbursement'] = SBA_data_copy['DaysToDisbursement'].astype('str').apply(
-        lambda x: x[:x.index('d') - 1]).astype('int64')
+    df_copy['DaysToDisbursement'] = \
+        df_copy['DaysToDisbursement'].astype('str').apply(lambda record: record[:record.index('d') - 1]).astype('int64')
 
     # Create StateSame flag field which identifies where the business State is the same as the BankState
-    SBA_data_copy['StateSame'] = np.where(SBA_data_copy['State'] == SBA_data_copy['BankState'], 1, 0)
+    df_copy['StateSame'] = np.where(df_copy['State'] == df_copy['BankState'], 1, 0)
 
     # Create SBA_AppvPct field since the guaranteed amount is based on a percentage of the gross loan amount
     # rather than dollar amount in most situations
-    SBA_data_copy['SBA_AppvPct'] = SBA_data_copy['SBA_Appv'] / SBA_data_copy['GrAppv']
+    df_copy['SBA_AppvPct'] = df_copy['SBA_Appv'] / df_copy['GrAppv']
 
     # Create AppvDisbursed flag field signifying if the loan amount disbursed was equal to the full amount approved
-    SBA_data_copy['AppvDisbursed'] = np.where(SBA_data_copy['DisbursementGross'] == SBA_data_copy['GrAppv'], 1, 0)
+    df_copy['AppvDisbursed'] = np.where(df_copy['DisbursementGross'] == df_copy['GrAppv'], 1, 0)
 
     # Format dtypes where necessary after feature engineering
-    SBA_data_copy = SBA_data_copy.astype({'RevLineCr': 'int64', 'LowDoc': 'int64', 'Default': 'int64',
-                                          'IsFranchise': 'int64', 'NewBusiness': 'int64', 'StateSame': 'int64',
-                                          'AppvDisbursed': 'int64'})
-
-    # Field for loans backed by Real Estate (loans with a term of at least 20 years)
-    SBA_data_copy['RealEstate'] = np.where(SBA_data_copy['Term'] >= 240, 1, 0)
-
-    # Field for loans active during the Great Recession (2007-2009)
-    SBA_data_copy['GreatRecession'] = np.where(((2007 <= SBA_data_copy['DisbursementFY']) &
-                                                (SBA_data_copy['DisbursementFY'] <= 2009)) |
-                                               ((SBA_data_copy['DisbursementFY'] < 2007) &
-                                                (SBA_data_copy['DisbursementFY'] +
-                                                 (SBA_data_copy['Term']/12) >= 2007)), 1, 0)
+    df_copy = df_copy.astype({'RevLineCr': 'int64', 'LowDoc': 'int64', 'Default': 'int64', 'IsFranchise': 'int64',
+                              'NewBusiness': 'int64', 'StateSame': 'int64', 'AppvDisbursed': 'int64'})
 
     # Remove unnecessary columns
-    SBA_data_copy.drop(columns=['LoanNr_ChkDgt', 'Name', 'Bank', 'NAICS', 'ApprovalDate', 'NewExist', 'FranchiseCode',
-                                'ChgOffDate', 'DisbursementDate', 'BalanceGross', 'ChgOffPrinGr', 'SBA_Appv'],
-                       inplace=True)
+    df_copy.drop(columns=['LoanNr_ChkDgt', 'Name', 'City', 'Zip', 'Bank', 'NAICS', 'ApprovalDate', 'NewExist',
+                          'FranchiseCode', 'ChgOffDate', 'NewExist', 'DisbursementDate', 'BalanceGross', 'ChgOffPrinGr',
+                          'SBA_Appv', 'MIS_Status'], inplace=True)
+
+    # Field for loans backed by Real Estate (loans with a term of at least 20 years)
+    df_copy['RealEstate'] = np.where(df_copy['Term'] >= 240, 1, 0)
+
+    # Field for loans active during the Great Recession (2007-2009)
+    df_copy['GreatRecession'] = np.where(
+        ((2007 <= df_copy['DisbursementFY']) & (df_copy['DisbursementFY'] <= 2009)) |
+        ((df_copy['DisbursementFY'] < 2007) & (df_copy['DisbursementFY'] + (df_copy['Term']/12) >= 2007)), 1, 0)
 
     # Only look at records with a DisbursementFY through 2010
-    SBA_data_copy = SBA_data_copy[SBA_data_copy['DisbursementFY'] <= 2010]
-
-    # Edit to keep LowDoc field since new time frame has more LowDoc records
-    # Remove LowDoc field
-    SBA_data_copy.drop(columns='LowDoc', inplace=True)
+    df_copy = df_copy[df_copy['DisbursementFY'] <= 2010]
 
     # Create flag to signify if a larger amount was disbursed than what the Bank had approved
-    SBA_data_copy['DisbursedGreaterAppv'] = np.where(SBA_data_copy['DisbursementGross'] > SBA_data_copy['GrAppv'], 1, 0)
+    df_copy['DisbursedGreaterAppv'] = np.where(df_copy['DisbursementGross'] > df_copy['GrAppv'], 1, 0)
 
     # Remove records with loans disbursed prior to being approved
-    SBA_data_copy = SBA_data_copy[SBA_data_copy['DaysToDisbursement'] >= 0]
+    df_copy = df_copy[df_copy['DaysToDisbursement'] >= 0]
 
     # Correlation Matrix
-    corr_matrix(df=SBA_data_copy)
+    corr_matrix(df=df_copy)
 
     # Exploring with Visualizations
     # Total/Average disbursed loan amount by industry
     # Create a groupby object on Industry for use in visualization
-    industry_group = SBA_data_copy.groupby(['Industry'])
+    industry_group = df_copy.groupby(['Industry'])
 
     # Data frames based on groupby Industry looking at aggregate and average values
     df_industrySum = industry_group.sum().sort_values('DisbursementGross', ascending=False)
@@ -216,8 +209,8 @@ def main():
     ax2a = plt.subplot(2, 1, 2)
 
     # Number of PIF and Defaulted loans by industry
-    stacked_setup(df=SBA_data_copy, col='Industry', axes=ax1a)
-    ax1a.set_xticklabels(SBA_data_copy.groupby(['Industry', 'Default'])['Industry'].count().unstack('Default').index,
+    stacked_setup(df=df_copy, col='Industry', axes=ax1a)
+    ax1a.set_xticklabels(df_copy.groupby(['Industry', 'Default'])['Industry'].count().unstack('Default').index,
                          rotation=35, horizontalalignment='right', fontsize=6)
 
     ax1a.set_title('Number of PIF/Defaulted Loans by Industry')
@@ -226,7 +219,7 @@ def main():
     ax1a.legend()
 
     # Number of PIF and Defaulted loans by State
-    stacked_setup(df=SBA_data_copy, col='State', axes=ax2a)
+    stacked_setup(df=df_copy, col='State', axes=ax2a)
 
     ax2a.set_title('Number of PIF/Defaulted Loans by State')
     ax2a.set_xlabel('State')
@@ -239,7 +232,7 @@ def main():
     # Paid in full and Defaulted loans by DisbursementFY
     fig4, ax4 = plt.subplots(figsize=(15, 5))
 
-    stack_data = SBA_data_copy.groupby(['DisbursementFY', 'Default'])['DisbursementFY'].count().unstack('Default')
+    stack_data = df_copy.groupby(['DisbursementFY', 'Default'])['DisbursementFY'].count().unstack('Default')
     x = stack_data.index
     y = [stack_data[1], stack_data[0]]
 
@@ -250,6 +243,61 @@ def main():
     ax4.legend(loc='upper left')
 
     plt.show()
+
+    # Paid in full and defaulted loans backed by Real Estate
+    fig5 = plt.figure(figsize=(20, 10))
+
+    ax1b = fig5.add_subplot(1, 2, 1)
+    ax2b = fig5.add_subplot(1, 2, 2)
+
+    stacked_setup(df=df_copy, col='RealEstate', axes=ax1b)
+    ax1b.set_xticks(df_copy.groupby(['RealEstate', 'Default'])['RealEstate'].count().unstack('Default').index)
+    ax1b.set_xticklabels(labels=['No', 'Yes'])
+
+    ax1b.set_title('Number of PIF/Defaulted Loans backed by Real Estate from 1984-2010', fontsize=15)
+    ax1b.set_xlabel('Loan Backed by Real Estate')
+    ax1b.set_ylabel('Number of Loans')
+    ax1b.legend()
+
+    # Paid in full and defaulted loans active during the Great Recession
+    stacked_setup(df=df_copy, col='GreatRecession', axes=ax2b)
+    ax2b.set_xticks(df_copy.groupby(['GreatRecession', 'Default'])['GreatRecession'].count().unstack('Default').index)
+    ax2b.set_xticklabels(labels=['No', 'Yes'])
+
+    ax2b.set_title('Number of PIF/Defaulted Loans Active during the Great Recession from 1984-2010', fontsize=15)
+    ax2b.set_xlabel('Loan Active during Great Recession')
+    ax2b.set_ylabel('Number of Loans')
+    ax2b.legend()
+
+    plt.show()
+
+    # Modeling
+    # One-hot encode categorical data
+    df_copy = pd.get_dummies(df_copy)
+
+    # Establish target and feature fields
+    y = df_copy['Default']
+    X = df_copy.drop('Default', axis=1)
+
+    # Scale the feature values prior to modeling
+    scale = StandardScaler()
+    X_scaled = scale.fit_transform(X)
+
+    # Split into training and testing sets
+    X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.25)
+
+    # Initialize models
+    log_reg = LogisticRegression(random_state=2)
+    xgboost = XGBClassifier(random_state=2)
+
+    # Modeling operations - Fit, predictions, print model performance metrics
+    # Logistic Regression
+    print("Logistic Regression esults:")
+    modeling(x_train=X_train, y_train=y_train, x_val=X_val, y_val=y_val, model=log_reg)
+
+    # XGBoost Classifier
+    print("XGBoost Classifier results:")
+    modeling(x_train=X_train, y_train=y_train, x_val=X_val, y_val=y_val, model=xgboost)
 
 
 def clean_str(rec) -> str:
@@ -289,6 +337,31 @@ def stacked_setup(df: pd.DataFrame, col: pd.Series, axes, stack_col: str = 'Defa
 
     axes.bar(data.index, data[1], label='Default')
     axes.bar(data.index, data[0], bottom=data[1], label='Paid in full')
+
+def modeling(x_train: pd.DataFrame, y_train: pd.Series, x_val: pd.DataFrame, y_val: pd.Series, model):
+    """
+    Creates a pipeline that performs feature selection to select the 10 most important features, fits a specified model
+    to the training data, makes a prediction, and prints a classification report with the results
+    :param x_train: Features used to train the model
+    :param y_train: Target used to train the model
+    :param x_val: Validation set of features used for predictions
+    :param y_val: Validation set of Target used for calculating model performance
+    :param model: Model object used for modeling - the type of model you'd like to use
+    :return: Classification report showing Precision, Recall, F1-Score, and accuracy for evaluating model performance
+    """
+
+    # Build pipeline for feature selection and modeling; SelectKBest defaults to top 10 important features
+    pipe = Pipeline(steps=[
+        ('feature_selection', SelectKBest()),
+        ('model', model)
+    ])
+
+    # Train te model and make predictions
+    pipe.fit(x_train, y_train)
+    y_pred = pipe.predict(x_val)
+
+    # Print the results
+    print(classification_report(y_val, y_pred, digits=3))
 
 
 if __name__ == '__main__':
